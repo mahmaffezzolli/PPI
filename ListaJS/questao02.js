3b12c15a5b225548570be9a3357e8245008d9d73
maria <mariaeduardamaffezzolli@gmail.com>
2023-10-31 18:18:37 -0300

    lista js

---
+++ ListaJS/questao02.js
@@ -0,0 +1,9 @@
+let a = prompt("Informe um número: ");
+let b = prompt("Informe outro número: ");
+
+a = parseFloat(a);
+b = parseFloat(b);
+
+let resultado = a + b;
+
+console.log("A soma é igual a: " + resultado);
---
+++ ListaJS/questao03.js
@@ -0,0 +1,5 @@
+let a = prompt("Informe um número: ");
+
+for(let i = 0; i < a; i++){
+    console.log(i);
+}
